16eff1092a416feeeecf5bac3817a6ba9aa770df
estagiario-dev2 <estagiario-dev2@escape79.com.br>
2024-07-18 15:19:39 -0300

    parei exatamente na aula 5-03

---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,7 @@
+import livro from '../../imagens/livro.png';
+import livro2 from '../../imagens/livro2.png';
+
+export const livros = [
+    {nome: 'Java 😖', id: 3, src:livro2},
+    {nome: 'PHP 😎😋', id: 4, src:livro},
+]
